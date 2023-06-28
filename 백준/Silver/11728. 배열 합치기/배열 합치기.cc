b55e3af4508599da520d1b0fa9cc410b8c7bd6aa
Floppa <91823355+Developdog@users.noreply.github.com>
2023-06-28 10:39:38 +0900

    Update 배열 합치기.cc
    배열 부정확

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -3,37 +3,35 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
 
     int n, m;
+    int a_index = 0, b_index = 0;
+
     cin >> n >> m;
 
-    int a[n], b[m];
+    int a[1000001] = { 0 }, b[1000001] = { 0 };
 
-    for (int i = 0; i < n; i++) {
+    for (int i=0;i<n;i++) {
         cin >> a[i];
     }
-    for (int i = 0; i < m; i++) {
+    for (int i=0;i<m;i++) {
         cin >> b[i];
     }
 
-    int a_index = 0, b_index = 0;
-    while (a_index < n && b_index < m) {
-        if (a[a_index] <= b[b_index]) {
+    while(a_index < n && b_index < m) {
+        if(a[a_index]<=b[b_index]) {
             cout << a[a_index++] << " ";
-        }
-        else {
+        } else {
             cout << b[b_index++] << " ";
         }
     }
-    while (a_index < n) {
+    while(a_index < n) {
         cout << a[a_index++] << " ";
     }
-    while (b_index < m) {
+    while(b_index < m) {
         cout << b[b_index++] << " ";
     }
-
-    return 0;
 }
