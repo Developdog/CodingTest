b54109cbb1c31824afa0d7d3d1a4372ee7d872e1
galrugsinote7 <91823355+Developdog@users.noreply.github.com>
2023-10-08 18:27:48 +0900

    [D2] Title: 아름이의 돌 던지기, Time: 7 ms, Memory: 13,536 KB -BaekjoonHub

---
+++ SWEA/D2/1285. 아름이의 돌 던지기/아름이의 돌 던지기.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int test_case;
+	int T;
+
+	cin >> T;
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{		
+		int n, answer = 0, answer_num = 100000, temp;
+
+		cin >> n;
+
+		for (int i = 0; i < n; i++)
+		{
+			cin >> temp;
+			temp = abs(temp);
+
+			if (temp < answer_num)
+			{
+				answer_num = temp;
+				answer = 1;
+			}
+			else if (temp == answer_num) answer++;
+		}
+
+		cout << "#" << test_case << " " << answer_num << " " << answer << "\n";
+	}
+	return 0;
+}
