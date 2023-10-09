f3105a2acf75c917594267bf018e645e8b248c4d
galrugsinote7 <91823355+Developdog@users.noreply.github.com>
2023-10-09 17:44:27 +0900

    [D2] Title: 시각 덧셈, Time: 7 ms, Memory: 13,336 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.cpp
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	int test_case;
 	int T;
-
 	cin >> T;
-
 	for (test_case = 1; test_case <= T; ++test_case)
 	{
-		int t1, m1, t2, m2;
-		cin >> t1 >> m1 >> t2 >> m2;
-        
-        m1 = m1 + m2;
-        t1 = t1 + t2 + m1 / 60;
-		m1 = m1 % 60;
-		
-        if(12 < t1) t1 = t1 - 12;
-        
-		cout << "#" << test_case << " " << t1 << " " << m1 << endl;
+		int ft, fm, st, sm;
+		cin >> ft >> fm >> st >> sm;
+
+		fm = fm + sm;
+		ft = ft + st + fm / 60;
+		fm %= 60;
+		if (ft % 12 == 0) ft = 12;
+		else ft %= 12;
+
+		cout << "#" << test_case << " " << ft << " " << fm << endl; 
 	}
 	return 0;
 }
