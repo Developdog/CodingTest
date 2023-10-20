b51961cff028e9c230f8cf72c9732bb587912949
galrugsinote7 <91823355+Developdog@users.noreply.github.com>
2023-10-20 14:31:44 +0900

    [D3] Title: 24시간, Time: 57 ms, Memory: 13,544 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T;
+    cin >> T;
+
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        int A, B;
+        cin >> A >> B;
+        cout << "#" << test_case << " ";
+        if (A + B >= 24) cout << (A + B) % 24;
+        else cout << A + B;
+        cout << endl;
+    }
+    return 0;
+}
