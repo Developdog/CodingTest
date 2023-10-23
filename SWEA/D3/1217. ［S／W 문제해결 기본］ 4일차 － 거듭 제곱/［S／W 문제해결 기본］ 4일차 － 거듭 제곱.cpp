eaf554d460bd19ce74bc7a23239824a962353d4e
galrugsinote7 <91823355+Developdog@users.noreply.github.com>
2023-10-23 15:39:23 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 6 ms, Memory: 13,536 KB -BaekjoonHub

---
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int test_case;
+    int T = 10;
+    //cin >> T;
+
+    for (test_case = 1; test_case <= T; ++test_case)
+    {
+        int N, M, ans, temp;
+        
+        cin >> temp >> N >> M;
+        ans = N;
+
+        for (int i = 0; i < M-1; i++) ans *= N;
+
+        cout << "#" << test_case << " " << ans << endl;
+    }
+    return 0;
+}
